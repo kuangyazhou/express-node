0b97c25b360cd0a9a751d9475e44a9681a9daf49
kuangyazhou <18707174713@139.com>
2017-08-04 18:30:59 +0800

    ''

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ app.get('/', function(req, res) {
     res.send("fuck the king!!!");
 });
 
+app.post('./', function(req, res) {
+    res.send('Got a post request');
+});
+
+app.put('./user', function(req, res) {
+    res.send('Got a Put request at /user');
+});
+
+app.delete('./user', function(req, res) {
+    res.send('Got a DELETE request at /user');
+});
+
 var server = app.listen(3000, function() {
     var host = server.address().address;
     var port = server.address().port;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+// 路由模块 访问http://localhost:3000/users
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 module.exports = router;
