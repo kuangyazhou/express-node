8148b1b2fb0f2ca3f36c00e08364d20ae32f121d
kuangyazhou <18707174713@139.com>
2017-08-30 14:15:55 +0800

    'filestream'

--- example.js
+++ example.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3001;
+const fs = require('fs');
 
 const buf1 = new Buffer('this is a test');
 console.log(buf1.toString());
@@ -10,6 +11,13 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Fuck the king!!');
 });
+
+
+//node 文件读取
+fs.readFile('./file/text.txt', function(Error, data) {
+    if (Error) { console.log(Error) };
+    console.log('异步读取' + data, data.toString());
+})
 
 server.listen(port, hostname, () => {
     console.log(`服务器运行在 http://${hostname}:${port}/`, 'fuck the king!!!');
